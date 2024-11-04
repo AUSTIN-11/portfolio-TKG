0e957389ccbd3ce8af6f006d407b91ed3927ac7f
Shedrack Okunda <okundashedrack@gmail.com>
2024-11-04 14:07:23 +0300

    styled the navbar for small screens

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = () => {
+  menu.classList.add("active");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabledScroll");
+};
+
+cancelBtn.onclick = () => {
+  menu.classList.remove("active");
+  menuBtn.classList.remove("hide");
+  body.classList.remove("disabledScroll");
+};
+
+window.onscroll = () => {
+  this.scrollY > 20
+    ? navbar.classList.add("sticky")
+    : navbar.classList.remove("sticky");
+};
+
 $(document).ready(function () {
   $(window).scroll(function () {
     // checks if window is scrolled more than 500px, adds/removes solid class
